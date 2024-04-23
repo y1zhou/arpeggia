1c0154ea2026b5f772d105f35e694808da1c493b
y1zhou <zhou.zy.yi@gmail.com>
2024-04-23 21:52:25 +0800

    feat(cli): add chain grouping; add more logs

--- src/interactions.rs
+++ src/interactions.rs
@@ -1,32 +1,77 @@
 use pdbtbx::*;
+use std::collections::HashSet;
 
 pub struct InteractionComplex {
     pub model: PDB,
+    pub ligand: HashSet<String>,
+    pub receptor: HashSet<String>,
     pub vdw_comp_factor: f64,
     pub interacting_threshold: f64,
 }
 
+impl InteractionComplex {
+    pub fn new(model: PDB, groups: &str, vdw_comp_factor: f64, interacting_threshold: f64) -> Self {
+        // Parse the first two fields in groups
+        let sel_vec: Vec<&str> = groups.split("/").collect();
+        let ligand_chains = sel_vec.get(0).unwrap();
+        let receptor_chains = sel_vec.get(1).unwrap();
+
+        // Create a HashSet of chains for the ligand and receptor
+        let mut ligand: HashSet<String> = ligand_chains
+            .split(",")
+            .map(|c| c.to_string())
+            .filter(|c| !c.is_empty())
+            .collect();
+        let mut receptor: HashSet<String> = receptor_chains
+            .split(",")
+            .map(|c| c.to_string())
+            .filter(|c| !c.is_empty())
+            .collect();
+
+        // If there are no ligand or receptor chains, use all remaining chains
+        if ligand.is_empty() ^ receptor.is_empty() {
+            if ligand.is_empty() {
+                ligand = model
+                    .chains()
+                    .map(|c| c.id().to_string())
+                    .filter(|c| !receptor.contains(c))
+                    .collect();
+            } else {
+                receptor = model
+                    .chains()
+                    .map(|c| c.id().to_string())
+                    .filter(|c| !ligand.contains(c))
+                    .collect();
+            }
+        }
+
+        println!("Parsed ligands {ligand:?}; receptors {receptor:?}");
+        Self {
+            model,
+            ligand,
+            receptor,
+            vdw_comp_factor,
+            interacting_threshold,
+        }
+    }
+}
+
 pub trait Interactions {
-    fn add_hydrogens(&self);
     fn initialize(&self);
     fn run_arpeggio(&self);
     fn get_contacts(&self);
 }
 
 impl Interactions for InteractionComplex {
-    fn add_hydrogens(&self) {
-        todo!()
-    }
-
-    fn get_contacts(&self) {
-        todo!()
-    }
-
     fn initialize(&self) {
         todo!()
     }
 
     fn run_arpeggio(&self) {
+        todo!()
+    }
+
+    fn get_contacts(&self) {
         todo!()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,26 @@ use crate::utils::load_model;
 use chains::ChainExt;
 use clap::Parser;
 use interactions::InteractionComplex;
+use std::collections::HashSet;
 use std::path::{Path, PathBuf};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Path to the file to be analyzed
+    /// Path to the PDB or mmCIF file to be analyzed
     #[arg(short, long)]
-    filename: PathBuf,
+    input: PathBuf,
 
     /// Output directory
     #[arg(short, long)]
-    output: String,
+    output: PathBuf,
 
-    /// Select the "ligand" for interactions, using selection syntax:
-    /// /<chain_id>/<res_num>[<ins_code>]/<atom_name> or
-    /// RESNAME:<het_id>. Fields can be omitted.
-    #[arg(short, long, num_args=0..)]
-    selection: Vec<String>,
+    /// Group chains for interactions:
+    /// e.g. A,B/C,D
+    /// where chains A and B are the "ligand" and C and D are the "receptor"
+    #[arg(short, long)]
+    groups: String,
 
     /// Compensation factor for VdW radii dependent interaction types
     #[arg(short = 'c', long = "vdw-comp", default_value_t = 0.1)]
@@ -35,16 +36,20 @@ struct Args {
     #[arg(short, long, default_value_t = 5.0)]
     dist_cutoff: f64,
 
-    /// Verbosity of the program
+    /// Verbosity of the program:
+    /// -v for info, -vv for debug, and -vvv for trace
     #[arg(short, long, action=clap::ArgAction::Count)]
     verbose: u8,
 }
 
 fn main() {
     let args = Args::parse();
+    if args.verbose > 2 {
+        println!("{args:?}");
+    }
 
-    // Make sure `filename` exists
-    let input_path = Path::new(&args.filename).canonicalize().unwrap();
+    // Make sure `input` exists
+    let input_path = Path::new(&args.input).canonicalize().unwrap();
     let input_file: String = input_path.to_str().unwrap().parse().unwrap();
 
     // Prepare output directory
@@ -52,29 +57,42 @@ fn main() {
     let output_path = Path::new(&args.output).canonicalize().unwrap();
     let _ = std::fs::create_dir_all(&output_path);
     let output_dir = output_path.to_str().unwrap();
-    if args.verbose > 0 {
+    if args.verbose > 1 {
         println!("Using input file {input_file}");
-        println!("Saving results to {output_dir}");
+        println!("Results will be saved to {output_dir}");
     }
 
     // Load file as complex structure
     let (pdb, errors) = load_model(&input_file);
-    if args.verbose > 1 {
+    if args.verbose > 0 {
         for err in errors {
             eprintln!("{err}");
         }
     }
 
-    if args.verbose > 0 {
+    // Information on the sequence of the chains in the model
+    if args.verbose > 1 {
         println!("Loaded {} chains", pdb.chain_count());
         for chain in pdb.chains() {
             println!(">{}\n{}", chain.id(), chain.pdb_seq().join(""));
         }
     }
 
-    let _i_complex = InteractionComplex {
-        model: pdb,
-        vdw_comp_factor: args.vdw_comp,
-        interacting_threshold: args.dist_cutoff,
-    };
+    // Debug information on the atom and residue types in the model
+    if args.verbose > 2 {
+        let mut atom_types = HashSet::new();
+        let mut res_types = HashSet::new();
+
+        for atom in pdb.atoms() {
+            atom_types.insert(atom.name());
+        }
+        for res in pdb.residues() {
+            res_types.insert(res.name().unwrap());
+        }
+
+        println!("Atom types: {atom_types:?}");
+        println!("{} Residue types: {res_types:?}", res_types.len());
+    }
+
+    let _i_complex = InteractionComplex::new(pdb, &args.groups, args.vdw_comp, args.dist_cutoff);
 }
