0c485abb7348ddf38426c1993aee2ef31a4cc170
y1zhou <zhou.zy.yi@gmail.com>
2024-05-03 11:07:35 +0800

    test(cli): unit test for chain group splitting

--- src/utils.rs
+++ src/utils.rs
@@ -19,6 +19,9 @@ pub fn parse_groups(
 ) -> (HashSet<String>, HashSet<String>) {
     // Parse the first two fields in groups
     let sel_vec: Vec<&str> = groups.split('/').collect();
+    if sel_vec.len() < 2 {
+        panic!("Invalid chain groups format!")
+    }
     let ligand_chains = sel_vec.first().unwrap();
     let receptor_chains = sel_vec.get(1).unwrap();
 
@@ -48,4 +51,64 @@ pub fn parse_groups(
     }
 
     (ligand, receptor)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn good_group_splits() {
+        let chains: HashSet<String> = HashSet::from(["A", "B", "C", "D"].map(|c| c.to_string()));
+
+        // All chains are specified
+        assert_eq!(
+            (
+                HashSet::from(["A", "B"].map(|c| c.to_string())),
+                HashSet::from(["C", "D"].map(|c| c.to_string()))
+            ),
+            parse_groups(&chains, "A,B/C,D")
+        );
+
+        // One chain missing (ignored in searches)
+        assert_eq!(
+            (
+                HashSet::from(["A"].map(|c| c.to_string())),
+                HashSet::from(["C", "D"].map(|c| c.to_string()))
+            ),
+            parse_groups(&chains, "A/C,D")
+        );
+
+        // Empty group
+        assert_eq!(
+            (
+                HashSet::from(["A", "B"].map(|c| c.to_string())),
+                HashSet::from(["C", "D"].map(|c| c.to_string()))
+            ),
+            parse_groups(&chains, "/C,D")
+        );
+        assert_eq!(
+            (
+                HashSet::from(["C"].map(|c| c.to_string())),
+                HashSet::from(["A", "B", "D"].map(|c| c.to_string()))
+            ),
+            parse_groups(&chains, "C/")
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid chain groups format!")]
+    fn empty_group_splits() {
+        // Nothing passed
+        let chains: HashSet<String> = HashSet::from(["A", "B", "C", "D"].map(|c| c.to_string()));
+        parse_groups(&chains, "");
+    }
+
+    #[test]
+    #[should_panic(expected = "Empty chain groups!")]
+    fn missing_groups_in_split() {
+        // Nothing passed
+        let chains: HashSet<String> = HashSet::from(["A", "B", "C", "D"].map(|c| c.to_string()));
+        parse_groups(&chains, "/");
+    }
 }
