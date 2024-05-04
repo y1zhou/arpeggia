907277a2c77ef100d585ceaf2f7693e0f5d236be
y1zhou <zhou.zy.yi@gmail.com>
2024-05-04 23:24:12 +0800

    refactor: re-export interactions module functions

--- src/interactions/mod.rs
+++ src/interactions/mod.rs
@@ -5,8 +5,10 @@ pub mod ionic;
 pub mod structs;
 pub mod vdw;
 
-use hbond::{find_hydrogen_bond, find_weak_hydrogen_bond};
-use hydrophobic::find_hydrophobic_contact;
-use ionic::find_ionic_bond;
-use structs::*;
-use vdw::find_vdw_contact;
+// Re-exports
+pub use complex::*;
+pub use hbond::{find_hydrogen_bond, find_weak_hydrogen_bond};
+pub use hydrophobic::find_hydrophobic_contact;
+pub use ionic::find_ionic_bond;
+pub use structs::*;
+pub use vdw::find_vdw_contact;
