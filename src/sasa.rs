25fbb5bc27278d6c26e2d3d95e9a417a5af5810e
y1zhou <zhou.zy.yi@gmail.com>
2024-08-14 12:25:20 +0800

    fix(sasa): ignore waters when calculating SASA

--- src/sasa.rs
+++ src/sasa.rs
@@ -1,6 +1,6 @@
+use crate::residues::ResidueExt;
 use pdbtbx::*;
 use polars::prelude::*;
-use rayon::prelude::*;
 use rust_sasa::calculate_sasa_internal;
 use rust_sasa::Atom as SASAAtom;
 
@@ -9,24 +9,29 @@ use crate::interactions::InteractingEntity;
 pub fn get_atom_sasa(pdb: &PDB) -> DataFrame {
     // Calculate the SASA for each atom
     let atoms = pdb
-        .par_atoms()
-        .map(|atom| SASAAtom {
+        .atoms_with_hierarchy()
+        .filter(|x| {
+            let resn = x.residue().resn().unwrap();
+            resn != "O" && resn != "X"
+        })
+        .map(|x| SASAAtom {
             position: nalgebra::Point3::new(
-                atom.pos().0 as f32,
-                atom.pos().1 as f32,
-                atom.pos().2 as f32,
+                x.atom().pos().0 as f32,
+                x.atom().pos().1 as f32,
+                x.atom().pos().2 as f32,
             ),
-            radius: atom
+            radius: x
+                .atom()
                 .element()
                 .unwrap()
                 .atomic_radius()
                 .van_der_waals
                 .unwrap() as f32,
-            id: atom.serial_number(),
+            id: x.atom().serial_number(),
             parent_id: None,
         })
         .collect::<Vec<_>>();
-    let atom_sasa = calculate_sasa_internal(&atoms, None, None);
+    let atom_sasa = calculate_sasa_internal(&atoms, None, Some(960));
 
     // Create a DataFrame with the results
     let atom_annotations = pdb
